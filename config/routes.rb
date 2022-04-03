01d0dc6d00c7a8ac747fe61214225d03dd982b5a
RVirk04 <randeepvirk29@gmail.com>
2022-04-03 06:15:00 -0500

    Created a registrations controller and modified the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'registrations'
+  }
   # get 'virk/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
