8e721b441c31e53d395e807ce76dd15a28d8b734
RVirk04 <randeepvirk29@gmail.com>
2022-04-28 09:42:53 -0500

    Added search bar along with category selection

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,21 +1,24 @@
 class Product < ApplicationRecord
-  # This AR object is linked with the products table.
-
-  # A product has a many to one relationship with a category.
-  # The products table has a category_id foreign key.
-  # In other words, a product belongs to a category.
-  # All columns within products table are - product_id, name, description, price, stock_quantity, category_id, created_at, updated_at.
-  # This products table contain a column named category_id which represents a reference to category table.
+  mount_uploader :picture, ProductPictureUploader
   belongs_to :category
-  belongs_to :user, optional: true
+  has_many :line_items, dependent: :destroy
+  has_many :orders, through: :line_items
 
-  mount_uploader :image, ProductImageUploader
-  serialize :image, JSON
+  validates :name, :description, :price, presence: true
+  validates :price, length: { maximum: 7 }
 
-  # Ensure that the product always includes a title, price and stock_quantity
-  validates :name, :price, :colour, :size, :details, :code, :sizeandfit, :lookafterme, :brand, presence: true
-  validates :name, uniqueness: true
-  validates :price, numericality: { only_integer: true }, length: { maximum: 7 }
-
-  COLOUR = %w{ Black White Navy Blue Red Clear Satin Yellow Seafoam }
+  def self.search(keyword, search_category)
+    # self.where('name LIKE ? and category_id = ?', "%#{keyword}%", search_category)
+    if keyword.present? || search_category.present?
+      if keyword && search_category == ""
+        where('name LIKE ?', "%#{keyword}%")
+      elsif keyword == "" && search_category != "All"
+        where('category_id = ?', "%#{search_category}%")
+      else
+        where(['name LIKE ? AND category_id = ?', "%#{keyword}%", search_category])
+      end
+    else
+      all
+    end
+  end
 end
--- app/views/products/_product.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! product, :id, :name, :price, :colour, :size, :details, :code, :brand, :sizeandfit, :look, :lookafterme, :aboutme, :category_id, :created_at, :updated_at
-json.url product_url(product, format: :json)
